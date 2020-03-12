d3ed8d79f3b8e7bbadd822e0aa3fc2b04072ce84
Imasha1998 <imasha.synotec@gmail.com>
2020-03-12 13:56:44 +0530

    #1 create index file

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h3>Name    : Imasha Madhuhansi Jayasinghe</h3>
+        <h3>Address : Ela- Ihala , Kiribathavila,Baddegama</h3>
+        <h3>School  : G/Christ Church Girls' College</h3>
+    </body>
+</html>
